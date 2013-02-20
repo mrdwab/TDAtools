40ed37e95693eb5e0f0ff37a5b3c26ff4e10c4cd
Ananda Mahto <mrdwab@gmail.com>
2013-02-20 13:56:40 +0530

    Cleanup

--- R/TDAtools-package.R
+++ R/TDAtools-package.R
@@ -2,13 +2,15 @@
 
 #'A collection of functions written for students at the Tata-Dhan Academy
 #'
-#'This package collects some functions that were written to be used by students at the Tata-Dhan Academy, particularly for their courses in quantitative techniques and research.
+#'This package collects some functions that were written to be used by students
+#'at the Tata-Dhan Academy, particularly for their courses in quantitative
+#'techniques and research.
 #'
 #'\tabular{ll}{ Package: \tab TDAtools\cr Type: \tab Package\cr Version: \tab
 #'1.0\cr Date: \tab 2013-02-12\cr License: \tab GPL-2\cr }
 #'
 #'@name TDAtools-package
-#'@aliases TDAtools-package TDAtools
+#'@aliases TDAtools TDAtools-package
 #'@docType package
 #'@author Ananda Mahto
 #'
