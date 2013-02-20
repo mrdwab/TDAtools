4d17885820225e9e7cfa0e936aef62c3017cd716
Ananda Mahto <mrdwab@gmail.com>
2013-02-20 17:09:08 +0530

    Minor changes to docs

--- R/TDAGroupMean.R
+++ R/TDAGroupMean.R
@@ -1,25 +1,28 @@
-#'Calculated the arithmetic mean of grouped data
+#'Estimate the arithmetic mean of grouped data
 #'
-#'This is essentially \code{\link{weighted.mean}} for a \code{data.frame} representing grouped data.
+#'This is essentially \code{\link{weighted.mean}} for a \code{data.frame}
+#'representing grouped data.
 #'
-#'@param inDF The input \code{data.frame}. This \code{data.frame} \emph{must} include the following as the first three columns (see \emph{Examples}):
-#'\itemize{
-#'\item The lower boundaries of the class
-#'\item The upper boundaries of the class
-#'\item The frequency for that class
-#'}
-#'@note It is also possible to call this function directly on the stored output of the \code{\link{TDAFreq}} function, though it would make more sense to calculate the simple arithmetic \code{\link{mean}} from the raw data if that is available
+#'
+#'@param inDF The input \code{data.frame}. This \code{data.frame} \emph{must}
+#'include the following as the first three columns (see \emph{Examples}):
+#'\itemize{ \item The lower boundaries of the class \item The upper boundaries
+#'of the class \item The frequency for that class }
+#'@note It is also possible to call this function directly on the stored output
+#'of the \code{\link{TDAFreq}} function, though it would make more sense to
+#'calculate the simple arithmetic \code{\link{mean}} from the raw data if that
+#'is available
 #'@author Ananda Mahto
-#'@seealso \code{\link{weighted.mean}}, \code{\link{mean}}, \code{\link{TDAFreq}} 
-#'
+#'@seealso \code{\link{weighted.mean}}, \code{\link{mean}},
+#'\code{\link{TDAFreq}}, \code{\link{TDAGroupMedian}}
 #'@examples
 #'
 #'## Your input data.frame should look like this
 #'
 #'MyGroupedDF <- data.frame(
-#'  ClassLow = c(0, 50, 100, 150, 200, 250, 300, 350, 400, 450),
-#'  ClassHigh = c(50, 100, 150, 200, 250, 300, 350, 400, 450, 500),
-#'  Freq = c(78, 122, 187, 82, 51, 47, 13, 9, 6, 4))
+#' ClassLow = c(0, 50, 100, 150, 200, 250, 300, 350, 400, 450),
+#' ClassHigh = c(50, 100, 150, 200, 250, 300, 350, 400, 450, 500),
+#' Freq = c(78, 123, 187, 82, 51, 47, 13, 9, 6, 4))
 #'MyGroupedDF
 #'TDAGroupMean(MyGroupedDF)
 #'
